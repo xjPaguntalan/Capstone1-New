e2053e9b0d3de6f6ca70c6cef6ac4b4264e59151
xjPaguntalan <124582214+xjPaguntalan@users.noreply.github.com>
2023-04-27 12:30:45 +0800

    Added Programming Languages page

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ ScrollReveal().reveal('.me #hi, .me #name, .me #desc, .me #hire', { delay: 10, o
 
 ScrollReveal().reveal('#aboutme, #fname, #desc2, #geninfo, #dob, #bp, #l, #jan, #dav, #eng, #contact, #numb, #insta, #fb, #link, #gmail', { delay: 10, origin: 'left', interval: 70 });
 
-ScrollReveal().reveal('#myPic, #myPic2', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('#myPic, #myPic2, #prog h1', { delay: 100, origin: 'right' });
+
+ScrollReveal().reveal('#prg1, #prg1 img, #txt1', {delay: 100, origin: 'left', interval: 100})
+
+ScrollReveal().reveal('#prg2, #prg2 img, #txt2', {delay: 100, origin: 'right', interval: 100})
+
+ScrollReveal().reveal('#prg3, #prg3 img, #txt3', {delay: 100, origin: 'left', interval: 100})
+
+
